3c257f8f025de00ddc27a6066f33cf1dc2c17825
sungtae <129718219+ainnot@users.noreply.github.com>
2024-06-11 22:42:52 +0900

    add a* and etc

---
+++ HPP_Game/Assets/Script/enemy/Enemy_Status.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Status : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameManager gameManager;
+    
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EnemyIdle()
+    {
+
+    }
+    private void FollowPlayer()
+    {
+        
+    }
+    private void AttackPlayer()
+    {
+
+    }
+}
