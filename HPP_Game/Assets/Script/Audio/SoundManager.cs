1e66b93667c351817c3d9b7e5cca7758835be077
roy0206 <swimkinig_ryu@naver.com>
2024-06-12 19:29:55 +0900

    사운드메니저 제작

---
+++ HPP_Game/Assets/Script/Audio/SoundManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    Dictionary<string, AudioClip> sounds = new Dictionary<string, AudioClip>();
+
+    List<AudioSource> currentSound = new();
+
+    private void Awake()
+    {
+        Instance = this;
+
+        var loadedSound = Resources.LoadAll<AudioClip>("");
+        foreach (var sound in loadedSound)
+        {
+            sounds.Add(sound.name, sound);
+        }
+    }
+
+    public void PlaySound(GameObject subject, string soundName, float time, bool loop )
+    {
+        AudioSource[] source = subject.GetComponents<AudioSource>();
+        AudioSource availableSource = null;
+        
+        foreach (var sound in source)
+        {
+            if (sound.isPlaying)
+            {
+                availableSource = sound;
+                break;
+            }
+        }
+
+        if(availableSource == null) availableSource = subject.AddComponent<AudioSource>();
+
+        availableSource.clip = sounds[soundName];
+        availableSource.loop = loop;
+        availableSource.Play();
+
+        currentSound.Add(availableSource);
+    }
+
+    public void StopSound(GameObject subject, string soundName)
+    {
+        currentSound.Find(sound => sound.name == soundName).Stop();
+    }
+    public void StopAllSounds()
+    {
+        while(currentSound.Count > 0)
+        {
+            currentSound[0].Stop();
+            currentSound.RemoveAt(0);
+        }
+    }
+
+
+    void Start()
+    {
+        PlaySound(GameObject.Find("Player"), "TaserGun", 1, false);
+    }
+
+    void Update()
+    {
+        int i = 0;
+        while(i < currentSound.Count)
+        {
+            var source = currentSound[i];
+            if(!source.isPlaying)
+            {
+                print("REMOVE!");
+                currentSound.RemoveAt(i);
+                continue;
+            }
+            i++;
+        }
+    }
+}
